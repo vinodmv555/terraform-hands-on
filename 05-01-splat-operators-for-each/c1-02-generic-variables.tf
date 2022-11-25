58e90055f7028fb06aacedfe66bae507464f4a84
Vinod MV <vinodmv555@gmail.com>
2022-11-25 17:01:38 +0530

    test

--- 05-01-splat-operators-for-each/c1-02-generic-variables.tf
+++ 05-01-splat-operators-for-each/c1-02-generic-variables.tf
@@ -19,7 +19,6 @@ variable "businessunit" {
 variable "environment" {
   description = "Enviornment"
   default     = "dev"
-<<<<<<< HEAD
 }
 
 # Instance Type - List
@@ -38,6 +37,4 @@ variable "instance_type_map" {
     "tst" = "t3.micro"
     "prd" = "t3.small"
   }
-=======
->>>>>>> 8005cff7d988589ba262f947f6309fc6d4fe6aa7
 }
--- 05-01-splat-operators-for-each/c1-02-local-variables.tf
+++ 05-01-splat-operators-for-each/c1-02-local-variables.tf
@@ -1,15 +1,8 @@
 locals {
   environment = var.environment
-<<<<<<< HEAD
   owners      = var.businessunit
   common_tags = {
     environment = local.environment
     owners      = local.owners
-=======
-  owners = var.businessunit
-  common_tags = {
-    environment = local.environment
-    owners = local.owners
->>>>>>> 8005cff7d988589ba262f947f6309fc6d4fe6aa7
   }
 }
